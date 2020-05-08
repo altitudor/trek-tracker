b8ddb051edf73a9657527f52e2de42538371da47
altitudor <monika.j.wilson@gmail.com>
2020-05-08 01:17:11 -0400

    Need to add tests

--- spec/models/trail_spec.rb
+++
@@ -1,6 +0,0 @@
-require "spec_helper"
-
-RSpec.describe Trail, type: :model do
-  it { should have_valid(:name).when("Trail1") }
-  it { should_not have_valid(:name).when(nil, "") }
-end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,8 +2,6 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
-    first_name { "first name"}
-    last_name { "last name"}
     sequence(:user_name) {|n| "user#{n}" }
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
