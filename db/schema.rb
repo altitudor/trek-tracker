74bc07efb5bbe7716e9f48533cc1084dafcb2093
altitudor <monika.j.wilson@gmail.com>
2020-05-07 23:54:29 -0400

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_205947) do
+ActiveRecord::Schema.define(version: 2020_05_08_030503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.text "note", null: false
+    t.string "hike_date"
+    t.bigint "user_id", null: false
+    t.bigint "trail_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trail_id"], name: "index_notes_on_trail_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "trails", force: :cascade do |t|
+    t.string "name", null: false
+    t.float "distance"
+    t.integer "ascent"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_trails_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +44,11 @@ ActiveRecord::Schema.define(version: 2020_05_04_205947) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
