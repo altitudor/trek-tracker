c54cb46fb953892cb9709176ef34728d2c847e3f
altitudor <monika.j.wilson@gmail.com>
2020-05-08 00:07:35 -0400

    fixed factory bot again

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,9 +3,9 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:user_name) { |n| "user#{n}"}
     password { 'password' }
     password_confirmation { 'password' }
-    user_name { "user#{n}"}
   end
 
 end
